7d9a388620c654a4fe4d4bbf1fe758a125b50966
Sergjio97 <sergiocizmja@gmail.com>
2022-01-20 17:12:48 +0100

    erroe axios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const app = new Vue ({
     el : "#app",
     data() {
-
+        return {
+            albumList: [],
+        }
+    },
+    
+    created() {
+        axios.get("http://localhost:8080/api-dischi/")
+        .then( (response) => {
+            this.albumList = response.data;
+        });
     }
 })
